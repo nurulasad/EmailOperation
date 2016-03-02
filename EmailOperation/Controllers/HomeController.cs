70d4fc753a64c7dbfea4ab7f526964548e1e9b9b
Asad <nurulasad@gratex.com.au>
2016-03-02 14:22:45 +1100

    update

--- EmailOperation/Controllers/HomeController.cs
+++ EmailOperation/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -26,5 +27,68 @@ namespace EmailOperation.Controllers
 
             return View();
         }
+
+        public string SendMail(MailConfig config)
+        {
+            MailMessage m = new MailMessage();
+            SmtpClient sc = new SmtpClient();
+
+            m.From = new MailAddress(config.from, config.from);
+            //m.To.Add(new MailAddress("nurulasad@gratex.com.au", "Display name To"));
+
+            m.To.Add(new MailAddress(config.to, config.to));
+
+            m.Subject = config.subject;
+            m.Body = config.body;
+
+            if (config.useWebConfigMailServer == bool.FalseString)
+            {
+                sc.Host = config.host;
+                sc.Port = int.Parse(config.port);
+            }
+            
+
+            if (config.useCredential == bool.TrueString)
+            {
+                sc.Credentials = new System.Net.NetworkCredential("Gratex\\svc.orq2email", "yUggQY1LbRA9PQJDFcOQ");
+            }
+            
+            if (config.useSSL == bool.TrueString)
+            {
+                sc.EnableSsl = true; // runtime encrypt the SMTP communications using SSL
+            }
+
+            try
+            {
+                sc.Send(m);
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+            
+            
+
+            return "Email sent";
+        }
+    }
+
+    public class MailConfig
+    {
+        public string from { get; set; }
+        public string to { get; set; }
+        public string subject { get; set; }
+        public string body { get; set; }
+        public string host { get; set; }
+        public string port { get; set; }
+
+
+        public string useWebConfigMailServer { get; set; }
+        public string useSSL { get; set; }
+        public string useCredential { get; set; }
+
+        public string username { get; set; }
+        public string password { get; set; }
+
     }
 }
--- EmailOperation/angular/mailController.js
+++ EmailOperation/angular/mailController.js
@@ -3,6 +3,64 @@
     .controller('mailController', ['$scope', '$http', mailController]);
 
     function mailController($scope, $http) {
-        console.log(11);
+        vm = $scope;
+
+        vm.useWebConfigMailServer = false;
+        vm.useSSL = true;
+        vm.useCredential = true;
+
+        vm.username = "Gratex\\svc.orq2email";
+        vm.password = "yUggQY1LbRA9PQJDFcOQ";
+
+
+        vm.host = "smtp.gratex.com.au";
+        vm.port = 25;
+
+
+        vm.fromAddress = "nurulasad@gratex.com.au";
+
+        vm.toAddress = "nurulasad@gratex.com.au";
+
+        vm.mailSubject = "Test";
+
+        vm.mailBody = "Mail body content";
+
+        vm.result = '';
+
+        vm.sendMail = function () {
+
+            var mail = {};
+
+            mail.useWebConfigMailServer = vm.useWebConfigMailServer;
+            mail.useSSL = vm.useSSL;
+            mail.useCredential = vm.useCredential;
+
+            mail.username = vm.username;
+            mail.password = vm.password;
+
+            mail.host = vm.host;
+            mail.port = vm.port;
+
+            mail.from = vm.fromAddress;
+            mail.to = vm.toAddress;
+            mail.subject = vm.mailSubject;
+            mail.body = vm.mailBody;
+
+           
+
+
+
+            $http.post('Home/SendMail', mail).then(function (result) {
+                vm.result = result;
+            }, function (error) {
+                vm.result = result;
+            });
+
+
+
+
+
+        }
+
     }
 })();
