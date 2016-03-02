8a178a565e8c4e6dcaba28142142fbed4151c7d9
Asad <nurulasad@gratex.com.au>
2016-03-02 12:54:30 +1100

    init

---
+++ EmailOperation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmailOperation.Startup))]
+namespace EmailOperation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ EmailOperation/angular/mailController.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    angular.module('mailApp')
+    .controller('mailController', ['$scope', '$http', mailController]);
+
+    function mailController($scope, $http) {
+        console.log(11);
+    }
+})();
